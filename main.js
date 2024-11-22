e263f0aefa4844ccebfc6d8c4ce6c50fd2109c7b
A-Deepak1610 <deepaka.ad24@bitsathy.ac.in>
2024-11-22 14:45:50 +0530

    done

---
+++ main.js
@@ -0,0 +1,12 @@
+function changeBoxColor(boxId, color){
+    document.getElementById(boxId).style.backgroundColor = color;
+}
+function changen1(){
+    document.getElementById("box1").style.backgroundColor = 'rgb(227, 227, 227)';
+}
+function changen2(){
+    document.getElementById("box2").style.backgroundColor = 'rgb(227, 227, 227)';
+}
+function changen3(){
+    document.getElementById("box3").style.backgroundColor = 'rgb(227, 227, 227)';
+}
